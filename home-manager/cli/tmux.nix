de1ffbf45d42194b13e9de06bc8c41705d39b198
Alireza Davoodi <alireza.davoodi@company.info>
2022-12-13 17:40:22 +0100

    added tmux plugin

--- home-manager/cli/tmux.nix
+++ home-manager/cli/tmux.nix
@@ -4,6 +4,12 @@
   programs.tmux = {
     enable = true;
     extraConfig = builtins.readFile "${inputs.tmux-conf}/.tmux.conf";
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+    ];
   };
 
   home.file.".tmux.conf.local" = { source = ./.tmux.conf.local; };
