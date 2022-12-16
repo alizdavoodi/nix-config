85336364fa8208cb30bbe5cbe54d72b875df0d5e
Alireza Davoodi <alireza.davoodi@company.info>
2022-12-16 10:14:02 +0100

    change flake

--- flake.nix
+++ flake.nix
@@ -9,13 +9,10 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
     hardware.url = "github:nixos/nixos-hardware";
     flake-utils.url = "github:numtide/flake-utils";
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
-    
+
+    comma.url = "github:nix-community/comma";
     vim-plugins = {
       url = "path:./modules";
     };
