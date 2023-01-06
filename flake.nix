e24c31f2b3edd3935b39390da7875ec7219101a3
Alireza Davoodi <alizdavoodi@gmail.com>
2023-01-06 15:38:19 +0100

    fixed flake url

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     # Delta is a syntax-highlighting pager for git, diff, and grep output.
     # NOTE: Include just for "themes.gitconfig" file 
     delta = { 
-      url = github:dandavison/delta; 
+      url = "github:dandavison/delta";
       flake = false; 
     };
 
