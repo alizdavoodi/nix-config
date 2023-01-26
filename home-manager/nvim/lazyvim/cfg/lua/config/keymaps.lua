97d2cf077a1280d229ad24930d628fb33987ca12
Alireza Davoodi <alizdavoodi@gmail.com>
2023-01-26 09:58:26 +0100

    config nvim keymaps

--- home-manager/nvim/lazyvim/cfg/lua/config/keymaps.lua
+++ home-manager/nvim/lazyvim/cfg/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--map("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>", { desc = "Open fzf-lua" })
+vim.api.nvim_set_keymap("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
