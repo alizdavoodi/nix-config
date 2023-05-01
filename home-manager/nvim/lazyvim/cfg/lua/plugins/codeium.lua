68bc4af78064f4b034e72b36745582ef7c81e984
Alireza Davoodi <alizdavoodi@gmail.com>
2023-05-01 09:18:39 +0200

    disabled codeium

---
+++ home-manager/nvim/lazyvim/cfg/lua/plugins/codeium.lua
@@ -0,0 +1,16 @@
+-- return {
+--   "Exafunction/codeium.vim",
+-- }
+
+-- return {
+--   "jcdickinson/codeium.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "hrsh7th/nvim-cmp",
+--   },
+--
+--   config = function()
+--     require("codeium").setup({})
+--   end,
+-- }
+return {}
