7c629f01059086499e79ff89ee260e1de6cca8d2
Alireza Davoodi <alizdavoodi@gmail.com>
2024-03-06 14:48:11 +0100

    feat(obsidian.nvim): just load plugin if the buffer is in vault folder

--- home-manager/nvim/lazyvim/cfg/lua/plugins/obsidian.lua
+++ home-manager/nvim/lazyvim/cfg/lua/plugins/obsidian.lua
@@ -4,12 +4,14 @@ return {
   lazy = true,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    "BufReadPre "
+      .. vim.fn.expand("~")
+      .. "/vaults/personal/**.md",
+    "BufReadPre " .. vim.fn.expand("~") .. "/vaults/work/**.md",
+  },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
