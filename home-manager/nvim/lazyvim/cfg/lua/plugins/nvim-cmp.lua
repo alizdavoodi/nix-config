bb11fa1d8dc392ac318868163bf9ac42b3bb8a87
Alireza Davoodi <alizdavoodi@gmail.com>
2024-03-11 18:29:43 +0100

    fix(nvim-cmp): disable <CR> mapping to prevent conflicts

--- home-manager/nvim/lazyvim/cfg/lua/plugins/nvim-cmp.lua
+++ home-manager/nvim/lazyvim/cfg/lua/plugins/nvim-cmp.lua
@@ -9,17 +9,19 @@ return {
     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "rg" } }))
     opts.mapping = cmp.config.mapping.preset.insert({
 
-      ["<CR>"] = cmp.mapping({
-        i = function(fallback)
-          if cmp.visible() and cmp.get_active_entry() then
-            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-          else
-            fallback()
-          end
-        end,
-        s = cmp.mapping.confirm({ select = true }),
-        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-      }),
+      ["<CR>"] = cmp.config.disable,
+
+      -- ["<CR>"] = cmp.mapping({
+      --   i = function(fallback)
+      --     if cmp.visible() and cmp.get_active_entry() then
+      --       cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+      --     else
+      --       fallback()
+      --     end
+      --   end,
+      --   s = cmp.mapping.confirm({ select = true }),
+      --   c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+      -- }),
     })
 
     opts.experimental = {
