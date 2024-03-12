dc4c0d8d35b73343bf340437dfbb3f0f0a065c2c
Alireza Davoodi <alizdavoodi@gmail.com>
2024-03-12 17:26:52 +0100

    feat(nvim): update gp-nvim and obsidian plugins with custom hooks and id generation

--- home-manager/nvim/lazyvim/cfg/lua/plugins/gp-nvim.lua
+++ home-manager/nvim/lazyvim/cfg/lua/plugins/gp-nvim.lua
@@ -1,9 +1,23 @@
 return {
   "robitx/gp.nvim",
   config = function()
-    require("gp").setup()
+    local config = {
+      hooks = {
+        Grammarly = function(gp, params)
+          local agent = gp.get_command_agent()
+          local template = "You are a machine that check grammar mistake and"
+            .. "make the sentence more fluent. You take all the user input and auto"
+            .. "correct it. Just reply to user input with correct grammar, DO NOT"
+            .. "reply the context of the question of the user input. If the user"
+            .. "input is grammatically correct and fluent, just reply with the exact same input.\n"
+            .. "Also respect any non alphabetical characters and leave them as they are.\n\n"
+            .. "```{{selection}}\n```\n\n"
 
-    -- or setup with your own config (see Install > Configuration in Readme)
-    -- require("gp").setup(config)
+          gp.Prompt(params, gp.Target.rewrite, nil, agent.model, template, agent.system_prompt)
+        end,
+      },
+    }
+
+    require("gp").setup(config)
   end,
 }
