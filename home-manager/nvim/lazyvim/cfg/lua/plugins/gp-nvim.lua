181c6f1c5d450478c9205b90b40b7b567f5e8079
Alireza Davoodi <alizdavoodi@gmail.com>
2024-03-14 10:52:20 +0100

    feat(gp.nvim): enforce wrap line length of 80 characters in plugins config

--- home-manager/nvim/lazyvim/cfg/lua/plugins/gp-nvim.lua
+++ home-manager/nvim/lazyvim/cfg/lua/plugins/gp-nvim.lua
@@ -10,7 +10,8 @@ return {
             .. "correct it. Just reply to user input with correct grammar, DO NOT"
             .. "reply the context of the question of the user input. If the user"
             .. "input is grammatically correct and fluent, just reply with the exact same input.\n"
-            .. "Also respect any non alphabetical characters and leave them as they are.\n\n"
+            .. "Also respect any non alphabetical characters and leave them as they are."
+            .. "Keep the wrap line lenght to 80 characters.\n\n"
             .. "```{{selection}}\n```\n\n"
 
           gp.Prompt(params, gp.Target.rewrite, nil, agent.model, template, agent.system_prompt)
