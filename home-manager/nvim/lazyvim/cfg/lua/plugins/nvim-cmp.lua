d4a92f1706cd5ed1ec710fe3f8470b768cc38822
Alireza Davoodi <alizdavoodi@gmail.com>
2024-03-26 12:12:50 +0100

    feat(nvim): added look cmp source

--- home-manager/nvim/lazyvim/cfg/lua/plugins/nvim-cmp.lua
+++ home-manager/nvim/lazyvim/cfg/lua/plugins/nvim-cmp.lua
@@ -2,11 +2,22 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "lukas-reineke/cmp-rg",
+    "octaltree/cmp-look",
   },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     local cmp = require("cmp")
-    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "rg" } }))
+    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+      { name = "rg" },
+      {
+        name = "look",
+        keyword_length = 2,
+        option = {
+          convert_case = true,
+          loud = true,
+        },
+      },
+    }))
     -- Disable preselect of auto-completion
     opts.preselect = cmp.PreselectMode.None
     opts.completion = {
