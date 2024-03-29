fea0bbb83340d4e0716e56141a7acfb942a79ce4
Alireza Davoodi <alizdavoodi@gmail.com>
2024-03-29 11:17:48 +0100

    feat(nvim): Added new Claude Opus model to gp.nvim (change the branch)

--- home-manager/nvim/lazyvim/cfg/lua/plugins/gp-nvim.lua
+++ home-manager/nvim/lazyvim/cfg/lua/plugins/gp-nvim.lua
@@ -1,5 +1,6 @@
 return {
   "robitx/gp.nvim",
+  branch = "copilot",
   config = function()
     local config = {
       agents = {
@@ -27,6 +28,35 @@ return {
           -- string with model name or table with model name and parameters
           model = { model = "gpt-4-turbo-preview", temperature = 0.8, top_p = 1 },
           -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = "You are an AI working as a code editor.\n\n"
+            .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+            .. "START AND END YOUR ANSWER WITH:\n\n```",
+        },
+        {
+          provider = "anthropic",
+          name = "ChatClaude-3-Opus",
+          chat = true,
+          command = false,
+          -- string with model name or table with model name and parameters
+          model = { model = "claude-3-opus-20240229", temperature = 0.8, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = "You are a general AI assistant.\n\n"
+            .. "The user provided the additional info about how they would like you to respond:\n\n"
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. "- Ask question if you need clarification to provide better answer.\n"
+            .. "- Think deeply and carefully from first principles step by step.\n"
+            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+            .. "- Use Socratic method to improve your thinking and coding skills.\n"
+            .. "- Don't elide any code from your output if the answer requires coding.\n"
+            .. "- Take a deep breath; You've got this!\n",
+        },
+        {
+          provider = "anthropic",
+          name = "CodeClaude-3-Opus",
+          chat = false,
+          command = true,
+          -- string with model name or table with model name and parameters
+          model = { model = "claude-3-opus-20240229", temperature = 0.8, top_p = 1 },
           system_prompt = "You are an AI working as a code editor.\n\n"
             .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
             .. "START AND END YOUR ANSWER WITH:\n\n```",
