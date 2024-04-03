a54cf47575eea3367a8383c84c4543186e278746
Alireza Davoodi <alizdavoodi@gmail.com>
2024-04-03 12:22:12 +0200

    feat(wezterm): integrate smart-splits with custom directional keys and modifiers

--- home-manager/cli/terminal/wezterm.lua
+++ home-manager/cli/terminal/wezterm.lua
@@ -17,10 +17,6 @@ config.default_gui_startup_args = { "connect", "unix" }
 
 -- Plugins
 --
--- nvim plugin smart-splits
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
--- you can put the rest of your Wezterm config here
-smart_splits.apply_to_config(config)
 
 -- Load the smart workspace switcher plugin
 local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
@@ -65,5 +61,22 @@ config.keys = {
 	},
 	{ key = "q", mods = "LEADER|CTRL", action = wezterm.action.QuickSelect },
 }
+
+-- nvim plugin smart-splits
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+-- you can put the rest of your Wezterm config here
+smart_splits.apply_to_config(config, {
+	-- the default config is here, if you'd like to use the default keys,
+	-- you can omit this configuration table parameter and just use
+	-- smart_splits.apply_to_config(config)
+
+	-- directional keys to use in order of: left, down, up, right
+	direction_keys = { "h", "j", "k", "l" },
+	-- modifier keys to combine with direction_keys
+	modifiers = {
+		move = "LEADER", -- modifier to use for pane movement, e.g. CTRL+h to move left
+		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
+	},
+})
 
 return config
