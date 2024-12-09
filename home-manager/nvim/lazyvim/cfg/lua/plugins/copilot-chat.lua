f0f7b168641b8a35afe641acb96f267fc99a8b0b
Alireza Davoodi <alizdavoodi@gmail.com>
2024-12-09 09:17:34 +0100

    feat(home-manager, copilot-chat): add lynx package and update CopilotChat.lua  with main branch and new model

--- home-manager/nvim/lazyvim/cfg/lua/plugins/copilot-chat.lua
+++ home-manager/nvim/lazyvim/cfg/lua/plugins/copilot-chat.lua
@@ -1,13 +1,14 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    branch = "main",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
     opts = {
       debug = false, -- Enable debugging
+      model = "claude-3.5-sonnet",
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
